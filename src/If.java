70275dee127c46efe7864e414a035a913a890703
user <iaborshev@gmail.com>
2024-02-18 13:23:46 +0300

    update

---
+++ src/If.java
@@ -0,0 +1,12 @@
+public class If {
+    public static void main(String[] args) {
+        int myInt = 5;
+
+        if(myInt < 5){
+            System.out.println("да, верно.");
+        } else if (myInt < 20) {
+            System.out.println("нет, не верно.");
+        }
+    }
+
+}
