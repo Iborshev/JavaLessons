7fb125122e6ca12cac3d3a8e0101b1dd1a583c7b
ilasborsev <iaborshev@gmail.com>
2024-02-24 18:35:31 +0300

    тест1

--- src/Arrays.java
+++ src/Arrays.java
@@ -9,7 +9,7 @@ public class Arrays {
             System.out.println(numbers[i]);
         }
         System.out.println();
-        int[] numbers1 = {1, 2, 3};
+        int[] numbers1 = {1, 2, 3, 4};
         for (int i = 0; i < numbers1.length; i++) {
             System.out.println(numbers1[i]);
 
--- src/Multidimensional_arrays.java
+++ src/Multidimensional_arrays.java
@@ -1,4 +1,4 @@
-
+git
     public class Multidimensional_arrays {
         public static void main(String[] args) {
             int[] numbers = {1, 2, 3};
